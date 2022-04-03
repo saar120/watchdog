81d91488c13f54958c863ac207fc0ae283ea4eca
Saar Amrani <saar120@gmail.com>
2022-04-03 12:02:31 +0300

    fix exec

--- utils/execUtil.js
+++ utils/execUtil.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 
 function getProcesses(searchTerm) {
     return new Promise((resolve, reject) => {
-        exec(`ps -ef kl;'jk| grep ${searchTerm}`, (err, stdout, stderr) => {
+        exec(`ps -ef | grep ${searchTerm}`, (err, stdout, stderr) => {
             if (err) {
                 reject(err);
             }
